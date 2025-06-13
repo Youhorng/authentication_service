6ab08c2d34f2608fb096ad159f0ee2dd8a2a8aba
Youhorng <2023221kean@aupp.edu.kh>
2025-06-13 23:39:42 +0700

    Fig user bug

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
